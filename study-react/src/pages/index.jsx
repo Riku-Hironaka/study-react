f311141cd968bcd55ea851736cad3465ad0cf61f
Riku-Hironaka <hironaka.riku1998@gmail.com>
2023-07-22 19:00:10 +0900

    onClickイベントを追加

--- study-react/src/pages/index.jsx
+++ study-react/src/pages/index.jsx
@@ -5,14 +5,27 @@ import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
+const foo =1;
+
+const handleClick =(e) => {  
+  console.log(e.target.href);
+  e.preventDefault();
+  alert(foo);
+}
 
 export default function Home() {
+
+
   return (
     <>
       <Head>
         <title>index pages</title>
       </Head>
-      <Header />
+      <Header/>
+      <a href='/about' onClick={handleClick}>
+          ボタン
+      </a>
+
       <Main title="Index Page" page="index" />
       <Footer />
     </>
